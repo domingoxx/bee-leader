5360c21d583612a3aa7e280dd7cbd32d9d24edf8
Peanut <wangchenxu92@gmail.com>
2021-06-01 21:10:41 +0800

    fix exit

--- bee-docker/start_bee.py
+++ bee-docker/start_bee.py
@@ -229,12 +229,14 @@ async def connectLeader():
       bee_process.terminate()
       bee_process.wait(5)
       if bee_process.returncode == None:
+        print(f'send kill bee {bee_process.pid}', flush=True)
         subprocess.check_output(f"kill -9 {bee_process.pid}", shell=True)
     if clef_process.returncode == None:
       print('send terminate clef', flush=True)
       clef_process.terminate()
       clef_process.wait(5)
       if clef_process.returncode == None:
+        print(f'send kill bee {clef_process.pid}', flush=True)
         subprocess.check_output(f"kill -9 {clef_process.pid}", shell=True)
     if not connection.closed:
       await connection.close()
